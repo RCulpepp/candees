a3b8d7acec37e0cfdfbb606c64a060b9a426929c
Ryan Culpepper <ryan.culpepper@gmail.com>
2018-10-13 11:04:24 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	document.onkeydown = function(e){
+		if(e.keyCode == 38){
+      console.log($('.character').prop('top'))
+			$('.character').prop('top', $('.character').attr('top')-1);
+		}
+	}
+
+
+});
+
+
